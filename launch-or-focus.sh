78b29ac3a2b768248e65d99f0503606e25de86de
Caio Encarnação de Queiroz <cencarnacaoq@gmail.com>
2026-03-18 10:22:43 -0300

    improve partial match on jq

--- launch-or-focus.sh
+++ launch-or-focus.sh
@@ -23,22 +23,28 @@ shift $((OPTIND - 1))
 
 WINDOW_PATTERN="$1"
 LAUNCH_COMMAND="${2:-"uwsm-app -- $WINDOW_PATTERN"}"
-WINDOW_ADDRESS=$(hyprctl clients -j | jq -r --arg p "$WINDOW_PATTERN" '  ($p | split("-")) as $parts
-  | .[]
-  | . as $client
-  | select(
-      any($parts[]; ($client.class | test(.;"i")) or ($client.title | test(.;"i")))
-    )|.address' | head -n1)
+WINDOW_ADDRESS=$(hyprctl clients -j | jq -r --arg p "$WINDOW_PATTERN" '
+  ($p | gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $norm_p |
+  .[] | 
+  ((.class // "") | gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $c |
+  ((.initialTitle // "") | gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $t |
+  select(
+    ($c | contains($norm_p)) or
+    ($t | contains($norm_p)) or
+    (($norm_p | contains($c)) and $c != "") or
+    (($norm_p | contains($t)) and $t != "")
+  ) | .address' | head -n1)
 
 if [ $DEBUG -eq 1 ]; then
-  echo $WINDOW_PATTERN
-  echo $LAUNCH_COMMAND
-  echo $WINDOW_ADDRESS
+  echo "$WINDOW_PATTERN"
+  echo "$LAUNCH_COMMAND"
+  echo "$WINDOW_ADDRESS"
   notify-send "$WINDOW_PATTERN - $WINDOW_ADDRESS"
+  exit 0
 fi
 
 if [[ -n $WINDOW_ADDRESS ]]; then
   hyprctl dispatch focuswindow "address:$WINDOW_ADDRESS"
 else
-  eval exec setsid $LAUNCH_COMMAND
+  eval exec setsid "$LAUNCH_COMMAND"
 fi
