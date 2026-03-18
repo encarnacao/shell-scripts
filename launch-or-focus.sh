d0f49a821b38761b8aa9e71ccb2fc48287dfd3be
Caio Encarnação de Queiroz <cencarnacaoq@gmail.com>
2026-03-18 16:58:15 -0300

    add logs for debugging to `launch-or-focus`

--- launch-or-focus.sh
+++ launch-or-focus.sh
@@ -21,13 +21,28 @@ done
 
 shift $((OPTIND - 1))
 
+create-log() {
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  {
+    echo "[$timestamp] Pattern: '$WINDOW_PATTERN', Possible Class: '$POSSIBLE_CLASS', Found Address: '$WINDOW_ADDRESS'"
+    echo "---------------------------CLASS_INFO------------------------------"
+    echo "$CLASS_INFO"
+    echo "--------------------------CLIENT_INFO------------------------------"
+    echo "$CLIENT_INFO"
+    echo "==================================================================="
+  } >>"$HOME/.logs/launch-or-focus.log"
+
+}
+
 WINDOW_PATTERN="$1"
 LAUNCH_COMMAND="${2:-"uwsm-app -- $WINDOW_PATTERN"}"
-POSSIBLE_CLASS=$(infer-hypr-class "$WINDOW_PATTERN" | jq '.possible_app_id')
-WINDOW_ADDRESS=$(hyprctl clients -j | jq -r --arg p "$WINDOW_PATTERN" --arg pc "$POSSIBLE_CLASS" '
+CLASS_INFO=$("$HOME/.local/bin/infer-hypr-class" "$WINDOW_PATTERN" 2>/dev/null || true)
+POSSIBLE_CLASS=$(printf '%s\n' "$CLASS_INFO" | jq -r '.possible_app_id // empty' 2>/dev/null)
+
+CLIENT_INFO=$(hyprctl clients -j | jq -r --arg p "$WINDOW_PATTERN" --arg pc "$POSSIBLE_CLASS" '
   ($p | gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $norm_p |
-  ($pc |  gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $norm_pc |
-  .[] | 
+  ($pc | gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $norm_pc |
+  .[] |
   ((.class // "") | gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $c |
   ((.title // "") | gsub("[^a-zA-Z0-9]";"") | ascii_downcase) as $t |
   select(
@@ -35,9 +50,13 @@ WINDOW_ADDRESS=$(hyprctl clients -j | jq -r --arg p "$WINDOW_PATTERN" --arg pc "
     ($t | contains($norm_p)) or
     (($norm_p | contains($c)) and $c != "") or
     (($norm_p | contains($t)) and $t != "") or
-    ($c | contains($norm_pc) and $norm_pc != "") or
-    ($norm_pc | contains($c) and $c != "")
-  ) | .address' | head -n1)
+    (($c | contains($norm_pc)) and $norm_pc != "") or
+    (($norm_pc | contains($c)) and $c != "")
+  )')
+
+WINDOW_ADDRESS=$(printf '%s\n' "$CLIENT_INFO" | jq -r '.address // empty' | head -n1)
+
+create-log
 
 if [ $DEBUG -eq 1 ]; then
   echo "$WINDOW_PATTERN"
